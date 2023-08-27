80e172c4f0f94fb440bebafb7df95f56832bf303
Deivid Hugo <deividhugoof@gmail.com>
2023-08-27 17:21:40 -0300

    Fixed AuthController Imports

---
+++ src/app/Auth/controllers/AuthController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+
+class AuthController {
+    async create(req: Request, res: Response): Promise<Response> {
+        return res.status(200).json({ message: 'Hello World' })
+    }
+
+    async destroy() {
+
+    }
+}
+
+export default new AuthController()
